9c1812cbe637ca595332219bfadfc4fa3bd6586f
CRAZYxRABBITxSUB <101494669+CRAZYxRABBITxSUB@users.noreply.github.com>
2022-03-13 16:01:36 +0900

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,26 +1,8 @@
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-    '/poster-keisuke.github.io/',
-];
-
-// インストール処理
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches
-            .open(CACHE_NAME)
-            .then(function(cache) {
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
 });
 
-// リソースフェッチ時のキャッシュロード処理
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches
-            .match(event.request)
-            .then(function(response) {
-                return response ? response : fetch(event.request);
-            })
-    );
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
 });
+self.addEventListener('fetch', function(event) {});
